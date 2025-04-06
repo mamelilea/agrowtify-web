d4931db65a9157c062e7f6405dde4df94e927ba7
mamelilea <achmalpradiptaaditama@gmail.com>
2025-04-06 19:36:13 +0700

    feat: add pages for login, register, home, about

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+    return (
+      <div className="py-6">
+        <h1 className="text-3xl font-bold text-center mb-6">Home</h1>
+      </div>
+    );
+  }
---
+++ src/app/about/page.tsx
@@ -0,0 +1,7 @@
+export default function AboutPage() {
+    return (
+      <div className="py-6">
+        <h1 className="text-3xl font-bold text-center mb-6">About</h1>
+      </div>
+    );
+  }
