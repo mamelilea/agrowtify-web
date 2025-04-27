c1de96b350693f177633d6fec0c477f2d3bd23ea
mamelilea <achmalpradiptaaditama@gmail.com>
2025-04-28 00:37:23 +0700

    feat: create general layout to support custom layouts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,37 +1,37 @@
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import React from 'react';
-import '@/styles/globals.css';
-import Navbar from '@/components/layouts/Navbar';
-import { AuthProvider } from '@/components/auth/AuthProvider';
+// app/layout.tsx (Root layout - dasar HTML)
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import React from "react";
+import "@/styles/globals.css";
+import { AuthProvider } from "@/components/auth/AuthProvider";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Agrowtify',
-  description: 'website petani',
+  title: "Agrowtify",
+  description: "website petani",
   icons: {
     icon: [
-      { url: '/favicon.ico' },
-      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
-      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: "/favicon.ico" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
     ],
     apple: {
-      url: '/apple-touch-icon.png',
-      type: 'image/png',
+      url: "/apple-touch-icon.png",
+      type: "image/png",
     },
     other: [
       {
-        rel: 'android-chrome-192x192',
-        url: '/android-chrome-192x192.png',
+        rel: "android-chrome-192x192",
+        url: "/android-chrome-192x192.png",
       },
       {
-        rel: 'android-chrome-512x512',
-        url: '/android-chrome-512x512.png',
+        rel: "android-chrome-512x512",
+        url: "/android-chrome-512x512.png",
       },
       {
-        rel: 'manifest',
-        url: '/site.webmanifest',
+        rel: "manifest",
+        url: "/site.webmanifest",
       },
     ],
   },
@@ -45,12 +45,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <AuthProvider>
-          <Navbar />
-          <main className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-            {children}
-          </main>
-        </AuthProvider>
+        <AuthProvider>{children}</AuthProvider>
       </body>
     </html>
   );
